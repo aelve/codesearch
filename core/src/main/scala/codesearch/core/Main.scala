c0d4e78c53a20a0a2db96dc21db00333ca8d6e5f
vlad107 <vlad10795@gmail.com>
2018-04-19 10:09:42 +0000

    Limit code search output

--- core/src/main/scala/codesearch/core/Main.scala
+++ core/src/main/scala/codesearch/core/Main.scala
@@ -33,8 +33,8 @@ object Main {
     }
   }
 
-  def csearch(query: String): Seq[String] = {
-    val answer = Seq("csearch", "-n", query).!!
+  def csearch(query: String): Seq[(String, String)] = {
+    val answer = (Seq("csearch", "-n", query) #| Seq("head", "-1000")) .!!
     answer.split('\n').map(toHackageLink).filterNot(_._1.isEmpty)
   }
 
