1a892644a9500ee68d2547a6c15fe84ba4e6834b
vlad107 <vlad10795@gmail.com>
2018-04-19 18:34:18 +0300

    Move to csearch to package

--- web-server/app/codesearch/web/controllers/Searcher.scala
+++ web-server/app/codesearch/web/controllers/Searcher.scala
@@ -1,17 +1,17 @@
 package codesearch.web.controllers
 
 import javax.inject.{Inject, Named}
+
 import play.api.mvc.{Action, InjectedController}
 
 import scala.concurrent.ExecutionContext
-
-import codesearch.core.Main
+import codesearch.core.utilities.IndexerUtility
 
 class Searcher @Inject() (
   implicit val executionContext: ExecutionContext
 ) extends InjectedController {
 
   def index(query: String) = Action { implicit request =>
-    Ok(views.html.search(Main.csearch(query)))
+    Ok(views.html.search(IndexerUtility.csearch(query)))
   }
 }
