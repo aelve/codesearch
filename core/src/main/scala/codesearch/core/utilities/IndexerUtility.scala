f1d8938c60c6ec71acc0b9c7d23efa85123e1afe
vlad107 <vlad10795@gmail.com>
2018-04-19 23:24:36 +0300

    Print context

--- core/src/main/scala/codesearch/core/utilities/IndexerUtility.scala
+++ core/src/main/scala/codesearch/core/utilities/IndexerUtility.scala
@@ -1,11 +1,14 @@
 package codesearch.core.utilities
 
+import scala.collection.mutable
+import scala.io.Source
 import sys.process._
 
-case class Result(link: String, nLine: String)
+case class Result(link: String, nLine: String, ctxt: Seq[String])
 case class PackageResult(verName: String, results: Seq[Result])
 
 object IndexerUtility {
+
   def csearch(query: String): Seq[PackageResult] = {
     val answer = (Seq("csearch", "-n", query) #| Seq("head", "-1000")) .!!
 
@@ -18,24 +21,40 @@ object IndexerUtility {
   def toHackageLink(uri: String): Option[(String, Result)] = {
     val elems: Seq[String] = uri.split(':')
     if (elems.length < 2) {
-      print(s"bad uri: $uri")
+      println(s"bad uri: $uri")
       None
     } else {
+      val fullPath = elems.head
       val pathSeq: Seq[String] = elems.head.split('/').drop(8)
       val nLine = elems.drop(1).head
       pathSeq.headOption match {
         case None =>
-          print(s"bad uri: $uri")
+          println(s"bad uri: $uri")
           None
         case Some(verName) =>
+          val rows = extractRows(fullPath, nLine.toInt)
+
           val remPath = pathSeq.drop(1).mkString("/")
 
           Some((verName, Result(
               s"https://hackage.haskell.org/package/$verName/src/$remPath",
-              nLine
+              nLine,
+              rows
           )))
       }
     }
+  }
+
+  def extractRows(path: String, i: Int): Seq[String] = {
+    val lines = Source.fromFile(path).getLines
+    val result = mutable.Buffer[String]()
+    lines.zipWithIndex.takeWhile(_._2 <= i+1).foreach {
+      case (line, ind) =>
+        if (ind >= i - 1) {
+          result.append(line)
+        }
+    }
+    result
   }
 
 }
