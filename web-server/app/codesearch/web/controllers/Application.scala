22807624f9aedecd44912d0443715ac5ce99410c
Vlad <vlad10795@gmail.com>
2018-05-01 01:32:25 +0300

    Move hackage part to /haskell, sources cheked by default

--- web-server/app/codesearch/web/controllers/Application.scala
+++ web-server/app/codesearch/web/controllers/Application.scala
@@ -9,6 +9,10 @@ class Application @Inject() (
   implicit val executionContext: ExecutionContext
 ) extends InjectedController {
 
+  def index_haskell = Action { implicit request =>
+    Ok(views.html.index_haskell())
+  }
+
   def index = Action { implicit request =>
     Ok(views.html.index())
   }
