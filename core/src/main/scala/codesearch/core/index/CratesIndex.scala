c3973bb37c3d90320a449b629adb2ae99c6909d7
Vlad <vlad10795@gmail.com>
2018-05-05 00:31:42 +0300

    Extract versions from database

--- core/src/main/scala/codesearch/core/index/CratesIndex.scala
+++ core/src/main/scala/codesearch/core/index/CratesIndex.scala
@@ -31,7 +31,6 @@ object CratesIndex extends Index with CratesDB {
   override def getLastVersions: Map[String, Version] = {
     val seq = Helper.recursiveListFiles(REPO_DIR.toIO).collect { case file if !(IGNORE_FILES contains file.getName) =>
       val lastVersionJSON = scala.io.Source.fromFile(file).getLines().toSeq.last
-      println(s"$file :: $lastVersionJSON")
       val obj = Json.parse(lastVersionJSON)
       val name = (obj \ "name").as[String]
       val vers = (obj \ "vers").as[String]
@@ -49,7 +48,7 @@ object CratesIndex extends Index with CratesDB {
       val fullPath = elems.head
       val pathSeq: Seq[String] = elems.head.split('/').drop(6)
       val nLine = elems.drop(1).head
-      val versions = getLastVersions.mapValues(_.verString)
+      val versions = Map(Await.result(CratesIndex.verNames(), Duration.Inf): _*)
       pathSeq.headOption match {
         case None =>
           println(s"bad uri: $uri")
