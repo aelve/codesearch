ce112f60282fbfe020b5030be3b711b07ab3f50b
Vlad <vlad10795@gmail.com>
2018-05-08 15:38:54 +0300

    Add sbt/clean

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ set -x
 ssh root@167.99.88.190 "
   cd /root/aelve/codesearch
   git pull
+  sbt web-server/clean
   sbt web-server/assembly
   mv codesearch-server.jar ../
   systemctl restart codesearch.service
