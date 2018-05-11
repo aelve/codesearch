98b71c3f78414a4fa18376b0caa400764ddd36e8
vlad107 <vlad10795@gmail.com>
2018-05-11 22:04:34 +0000

    Change path

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,6 @@ ssh root@167.99.88.190 "
   set -x
   cd /root/aelve/codesearch
   git pull
-  sbt web-server/clean
   sbt web-server/assembly
-  mv codesearch-server.jar ../
   systemctl restart codesearch.service
 "
