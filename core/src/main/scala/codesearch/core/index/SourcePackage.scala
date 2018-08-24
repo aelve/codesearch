b8f5b8c874aae0d07128ad9c462bb4f5d7d27bea
kamilongus <kamilongus@yandex.ru>
2018-08-24 15:12:32 +0500

    Issue: https://github.com/aelve/codesearch/issues/74

--- core/src/main/scala/codesearch/core/index/SourcePackage.scala
+++ core/src/main/scala/codesearch/core/index/SourcePackage.scala
@@ -1,17 +1,17 @@
 package codesearch.core.index
 
+import sys.process._
 import java.io.File
-import java.net.URI
+import java.net.{URI, URLEncoder}
 import java.nio.file.{Path, Paths}
 
 import codesearch.core.util.Helper
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import sys.process._
 
 trait Extractor {
-  def extractor(archive: File, directory: File): Future[File] =
+  def extract(archive: File, directory: File): Future[File] =
     Future {
       Seq("tar", "-xvf", archive.getCanonicalPath, "-C", directory.getCanonicalPath) !!;
       directory
@@ -39,20 +39,22 @@ case class GemPackage(
   val fsPath: Path                     = Paths.get(new URI(s"./gem/$name/$version"))
   val url: URI                         = URI.create(s"https://rubygems.org/downloads/$name-$version.gem")
   override def extentions: Set[String] = Helper.langByExt.keySet
-  override def extractor(archive: File, directory: File): Future[File] = {
+  override def extract(archive: File, directory: File): Future[File] = {
     Future {
-      Seq("gem", "unpack", s"--target=${directory.getCanonicalFile}", archive.getCanonicalFile) !!;
+      Seq("gem", "unpack", s"--target=${directory.getCanonicalPath}", archive.getCanonicalPath) !!;
       directory
     }
   }
 }
 
 case class NpmPackage(
-    name: String,
+    rawName: String,
     version: String
 ) extends SourcePackage {
-  val fsPath: Path                     = Paths.get(new URI(s"./hackage/$name/$version"))
-  val url: URI                         = URI.create(s"https://hackage.haskell.org/package/$name-$version/$name-$version.tar.gz")
+  //Because package name can look like: react>>=native@@router!!v2.1.123(refactored:-))
+  val name: String                     = URLEncoder.encode(rawName, "UTF-8")
+  val fsPath: Path                     = Paths.get(new URI(s"./npm/$name/$version"))
+  val url: URI                         = URI.create(s"https://registry.npmjs.org/$name/-/$name-$version.tgz")
   override def extentions: Set[String] = Set("js", "json", "xml", "yml", "coffee", "markdown", "md", "yaml", "txt")
 }
 
