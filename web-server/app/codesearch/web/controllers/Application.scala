67e4be1d74352559698a25235ba7a2001833caa2
Vladimir Alekseev <som3t1me5@gmail.com>
2018-08-25 10:35:24 +0300

    issue #71 fix errors in web

--- web-server/app/codesearch/web/controllers/Application.scala
+++ web-server/app/codesearch/web/controllers/Application.scala
@@ -1,6 +1,6 @@
 package codesearch.web.controllers
 
-import codesearch.core.index.{CratesIndex, GemIndex, HackageIndex, NpmIndex}
+import codesearch.core.index.{HaskellIndex, JavaScriptIndex, RubyIndex, RustIndex}
 import com.github.marlonlom.utilities.timeago.TimeAgo
 import javax.inject.Inject
 import play.api.mvc.InjectedController
@@ -16,35 +16,35 @@ class Application @Inject() (
 ) extends InjectedController {
 
   def haskell = Action.async { implicit request =>
-    HackageIndex.updated.map(updated => Ok(views.html.haskell(
+    HaskellIndex().updated.map(updated => Ok(views.html.haskell(
       TimeAgo.using(updated.getTime)
     )))
   }
 
   def rust = Action.async { implicit request =>
-    CratesIndex.updated.map(updated => Ok(views.html.rust(
+    RustIndex().updated.map(updated => Ok(views.html.rust(
       TimeAgo.using(updated.getTime)
     )))
   }
 
   def javascript = Action.async { implicit request =>
-    NpmIndex.updated.map(updated => Ok(views.html.javascript(
+    JavaScriptIndex().updated.map(updated => Ok(views.html.javascript(
       TimeAgo.using(updated.getTime)
     )))
   }
 
   def ruby = Action.async { implicit request =>
-    GemIndex.updated.map(updated => Ok(views.html.ruby(
+    RubyIndex().updated.map(updated => Ok(views.html.ruby(
       TimeAgo.using(updated.getTime)
     )))
 
   }
 
   def index = Action.async { implicit request =>
-    HackageIndex.updated.zip(HackageIndex.getSize)
-      .zip(CratesIndex.updated.zip(CratesIndex.getSize))
-      .zip(NpmIndex.updated.zip(NpmIndex.getSize))
-      .zip(GemIndex.updated.zip(GemIndex.getSize))
+    HaskellIndex().updated.zip(HaskellIndex().getSize)
+      .zip(RustIndex().updated.zip(RustIndex().getSize))
+      .zip(JavaScriptIndex().updated.zip(JavaScriptIndex().getSize))
+      .zip(RubyIndex().updated.zip(RubyIndex().getSize))
       .map {
       case ((((updatedHackage, sizeHackage), (updatedCrates, sizeCrates)), (updatedNpm, sizeNpm)), (updatedGem, sizeGem)) =>
         Ok(views.html.index(
