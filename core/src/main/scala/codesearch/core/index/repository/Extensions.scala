28631d476ee74141a40ca803ed3cd46fc24f2b3c
kamilongus <kamilongus@yandex.ru>
2018-09-04 13:27:49 +0500

    + common extensions separately from source extensions Issue: https://github.com/aelve/codesearch/issues/93

--- core/src/main/scala/codesearch/core/index/repository/Extensions.scala
+++ core/src/main/scala/codesearch/core/index/repository/Extensions.scala
@@ -3,26 +3,29 @@ package codesearch.core.index.repository
 import simulacrum.typeclass
 
 @typeclass trait Extensions[A] {
-  def commonExtensions: Set[String] =
-    Set("json", "cabal", "md", "txt", "xml", "yml", "yaml", "properties")
-  def extensions: Set[String]
+  def commonExtensions: Set[String] = Set("json", "cabal", "md", "txt", "xml", "yml", "yaml", "properties")
+  def sourceExtensions: Set[String]
+  def extensions: Set[String] = commonExtensions ++ sourceExtensions
 }
 
 object Extensions {
 
   implicit def jsExtensions[A <: JavaScript]: Extensions[A] = new Extensions[A] {
-    override def extensions: Set[String] = commonExtensions ++ Set("js", "ts", "json", "coffee")
+    override def commonExtensions: Set[String] =
+      super.commonExtensions ++ Set("css", "scss", "postcss", "sass", "less", "stylus", "html", "xhtml")
+    override def sourceExtensions: Set[String] = Set("js", "ts", "coffee", "jsx")
+    override def extensions: Set[String]       = commonExtensions ++ sourceExtensions
   }
 
   implicit def haskellExtensions[A <: Haskell]: Extensions[A] = new Extensions[A] {
-    override def extensions: Set[String] = commonExtensions ++ Set("hs", "lhs", "hsc", "hs-boot", "lhs-boot")
+    override def sourceExtensions: Set[String] = Set("hs", "lhs", "hsc", "hs-boot", "lhs-boot")
   }
 
   implicit def rubyExtensions[A <: Ruby]: Extensions[A] = new Extensions[A] {
-    override def extensions: Set[String] = commonExtensions ++ Set("rb", "rbx", "irb")
+    override def sourceExtensions: Set[String] = Set("rb", "rbx", "irb")
   }
 
   implicit def rustExtensions[A <: Rust]: Extensions[A] = new Extensions[A] {
-    override def extensions: Set[String] = commonExtensions ++ Set("rs")
+    override def sourceExtensions: Set[String] = commonExtensions ++ Set("rs")
   }
 }
