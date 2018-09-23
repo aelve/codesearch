26b8e121d3a64a66010629d8d174fcf45397358c
kamilongus <kamilongus@yandex.ru>
2018-09-23 21:55:51 +0500

    + refactor Issue: https://github.com/aelve/codesearch/issues/55

--- core/src/main/scala/codesearch/core/index/details/NpmDetails.scala
+++ core/src/main/scala/codesearch/core/index/details/NpmDetails.scala
@@ -2,8 +2,10 @@ package codesearch.core.index.details
 
 import java.nio.ByteBuffer
 import java.nio.file.StandardOpenOption.TRUNCATE_EXISTING
+import java.nio.file.StandardOpenOption.CREATE
 import java.nio.file.{Path, Paths}
 
+import cats.Semigroup
 import cats.effect.IO
 import codesearch.core.index.repository.DownloadException
 import codesearch.core.model.Version
@@ -17,7 +19,6 @@ import io.circe.syntax._
 import io.circe.generic.auto._
 import org.slf4j.{Logger, LoggerFactory}
 import cats.implicits._
-import cats.kernel.Monoid
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.ExecutionContext
@@ -25,9 +26,7 @@ import scala.language.higherKinds
 import codesearch.core.index.directory.PathOps._
 import codesearch.core.index.details.NpmDetails.FsIndexRoot
 
-private final case class Doc(name: String, tag: LatestTag)
 private final case class NpmRegistryPackage(name: String, version: String)
-private final case class LatestTag(latest: String) extends AnyVal
 private final case class NpmPackage(name: String, version: String)
 
 private[index] final class NpmDetails(implicit ec: ExecutionContext, http: SttpBackend[IO, Stream[IO, ByteBuffer]]) {
@@ -37,21 +36,15 @@ private[index] final class NpmDetails(implicit ec: ExecutionContext, http: SttpB
   private val NpmRegistryUrl = uri"https://replicate.npmjs.com/_all_docs?include_docs=true"
   private val FsIndexPath    = FsIndexRoot / "npm_packages_index.json"
 
-  private implicit val docDecoder: Decoder[Doc] = (c: HCursor) =>
+  private implicit val versionSemigroup: Semigroup[Version] = (_, last) => last
+
+  private implicit val docDecoder: Decoder[NpmRegistryPackage] = (c: HCursor) => {
+    val doc = c.downField("doc")
     for {
-      name <- c.get[String]("name")
-      tag  <- c.get[LatestTag]("dist-tags")
-    } yield Doc(name, tag)
-
-  private implicit val npmRegistryPackageDecoder: Decoder[NpmRegistryPackage] =
-    (c: HCursor) => c.get[Doc]("doc").map(doc => NpmRegistryPackage(doc.name, doc.tag.latest))
-
-  private implicit val mapMonoid: Monoid[Map[String, Version]] = new Monoid[Map[String, Version]] {
-    override def empty: Map[String, Version] = Map.empty
-    override def combine(
-        x: Map[String, Version],
-        y: Map[String, Version]
-    ): Map[String, Version] = x ++ y
+      name <- doc.get[String]("name")
+      distTag = doc.downField("dist-tags")
+      tag <- distTag.get[String]("latest")
+    } yield NpmRegistryPackage(name, tag)
   }
 
   def index: IO[Unit] =
@@ -76,10 +69,6 @@ private[index] final class NpmDetails(implicit ec: ExecutionContext, http: SttpB
       .foldMonoid
   }
 
-  private def toMap[F[_]]: Pipe[IO, NpmPackage, Map[String, Version]] = { input =>
-    input.map(npmPackage => Map(npmPackage.name -> Version(npmPackage.version)))
-  }
-
   private def stream(url: Uri): IO[Stream[IO, ByteBuffer]] = {
     sttp
       .get(url)
@@ -91,6 +80,10 @@ private[index] final class NpmDetails(implicit ec: ExecutionContext, http: SttpB
 
   private def toBytes[F[_]]: Pipe[IO, ByteBuffer, Byte] = { input =>
     input.flatMap(buffer => Stream.chunk(Chunk.array(buffer.array)))
+  }
+
+  private def toMap[F[_]]: Pipe[IO, NpmPackage, Map[String, Version]] = { input =>
+    input.map(npmPackage => Map(npmPackage.name -> Version(npmPackage.version)))
   }
 
   private def cutStream: Pipe[IO, Byte, Byte] = { input =>
@@ -116,7 +109,7 @@ private[index] final class NpmDetails(implicit ec: ExecutionContext, http: SttpB
     input.flatMap(registryPackage => Stream.chunk(Chunk.array(registryPackage.asJson.noSpaces.getBytes :+ '\n'.toByte)))
   }
 
-  private def toFile: Sink[IO, Byte] = file.writeAllAsync(FsIndexPath, List(TRUNCATE_EXISTING))
+  private def toFile: Sink[IO, Byte] = file.writeAllAsync(FsIndexPath, List(CREATE, TRUNCATE_EXISTING))
 
 }
 
