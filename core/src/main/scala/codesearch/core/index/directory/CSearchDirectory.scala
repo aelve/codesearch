8faef909a01b9a70c055d0e394a573c3bc52e3fd
kamilongus <kamilongus@yandex.ru>
2018-10-28 18:10:20 +0400

    + fixed canonical path Issue: https://github.com/aelve/codesearch/issues/138

--- core/src/main/scala/codesearch/core/index/directory/CSearchDirectory.scala
+++ core/src/main/scala/codesearch/core/index/directory/CSearchDirectory.scala
@@ -54,8 +54,9 @@ object DirAs {
   private val root: Path = Paths.get("./index/csearch/")
 
   implicit def asString: DirAs[String] = new DirAs[String] {
-    override def dir(packageManager: String): String     = asPath.dir(packageManager).toFile.getCanonicalPath
-    override def tempDir(packageManager: String): String = asPath.tempDir(packageManager).toFile.getAbsolutePath
+    private def fullPath(relativePath: Path): String     = relativePath.toFile.getCanonicalPath
+    override def dir(packageManager: String): String     = fullPath(asPath.dir(packageManager))
+    override def tempDir(packageManager: String): String = fullPath(asPath.tempDir(packageManager))
   }
 
   implicit def asPath: DirAs[Path] = new DirAs[Path] {
