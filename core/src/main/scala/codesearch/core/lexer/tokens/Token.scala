9dd64294192eb2dc7dbc089421de163ae70b500c
evgeny <evengining@yandex.ru>
2018-11-04 17:52:49 +0300

    Edit format code

--- core/src/main/scala/codesearch/core/lexer/tokens/Token.scala
+++ core/src/main/scala/codesearch/core/lexer/tokens/Token.scala
@@ -2,8 +2,8 @@ package codesearch.core.lexer.tokens
 
 trait Token {
   def repr: String = this match {
-    case Other(valueToken) => valueToken.toString
+    case Other(valueToken)         => valueToken.toString
     case SpecialSymbol(valueToken) => valueToken.toString
-    case Content(valueToken) => valueToken.toString
+    case Content(valueToken)       => valueToken.toString
   }
 }
