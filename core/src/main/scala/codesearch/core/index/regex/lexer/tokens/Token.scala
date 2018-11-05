1f32b02fa81071df39a9244e19c3df48bbc901a3
evgeny <evengining@yandex.ru>
2018-11-05 04:18:29 +0300

    Fixed tokens to satisfy CicrleCI

--- core/src/main/scala/codesearch/core/index/regex/lexer/tokens/Token.scala
+++ core/src/main/scala/codesearch/core/index/regex/lexer/tokens/Token.scala
@@ -2,14 +2,14 @@ package codesearch.core.index.regex.lexer.tokens
 
 trait Token {
   def repr: String = this match {
-    case Other(valueToken)         => valueToken.toString
+    case Other(valueToken) => valueToken.toString
     case SpecialSymbol(valueToken) => valueToken.toString
-    case Content(valueToken)       => valueToken.toString
+    case Content(valueToken) => valueToken.toString
   }
 }
 
-final case class Other(value: String)       extends Token
+final case class Other(value: String) extends Token
 
-final case class Content(value: String)     extends Token
+final case class Content(value: String) extends Token
 
 final case class SpecialSymbol(value: Char) extends Token
