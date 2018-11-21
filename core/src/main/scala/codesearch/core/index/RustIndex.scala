419b165fa8909e22af2711fb61f888026e5687fe
kamilongus <kamilongus@yandex.ru>
2018-11-22 03:53:53 +0400

    Issue: https://github.com/aelve/codesearch/issues/152

--- core/src/main/scala/codesearch/core/index/RustIndex.scala
+++ core/src/main/scala/codesearch/core/index/RustIndex.scala
@@ -1,26 +1,29 @@
 package codesearch.core.index
 
+import java.io.File
 import java.nio.ByteBuffer
 import java.nio.file.Path
 
-import ammonite.ops.pwd
 import cats.effect.{ContextShift, IO}
 import codesearch.core.config.{Config, RustConfig}
 import codesearch.core.db.CratesDB
 import codesearch.core.index.directory.Directory._
 import codesearch.core.index.directory.Directory.ops._
-import codesearch.core.index.repository.CratesPackage
 import codesearch.core.index.directory.СSearchDirectory
 import codesearch.core.index.repository.Extensions._
+import codesearch.core.index.repository.{CratesPackage, FileDownloader}
 import codesearch.core.model
 import codesearch.core.model.{CratesTable, Version}
 import codesearch.core.util.Helper
-import com.softwaremill.sttp.SttpBackend
+import com.softwaremill.sttp.{SttpBackend, _}
 import fs2.Stream
+import org.apache.commons.io.FileUtils
+import org.rauschig.jarchivelib.ArchiveFormat.ZIP
+import org.rauschig.jarchivelib.ArchiverFactory
 import play.api.libs.json.Json
+import codesearch.core.index.directory.Preamble._
 
 import scala.concurrent.ExecutionContext
-import scala.sys.process._
 
 class RustIndex(rustConfig: RustConfig)(
     implicit val executor: ExecutionContext,
@@ -28,12 +31,12 @@ class RustIndex(rustConfig: RustConfig)(
     val shift: ContextShift[IO]
 ) extends LanguageIndex[CratesTable] with CratesDB {
 
-  private val REPO_DIR = pwd / 'data / 'rust / "crates.io-index"
-  private val IGNORE_FILES = Set(
+  private val GithubUrl = uri"https://github.com/rust-lang/crates.io-index/archive/master.zip"
+  private val RepoDir   = new File("./data/rust")
+  private val IgnoreFiles = Set(
     "test-max-version-example-crate",
     "version-length-checking-is-overrated",
-    "config.json",
-    ".git"
+    "config.json"
   )
 
   override protected type Tag = Rust
@@ -46,21 +49,19 @@ class RustIndex(rustConfig: RustConfig)(
     archiveDownloadAndExtract(CratesPackage(name, version))
   }
 
-  override def downloadMetaInformation: IO[Unit] = IO {
-    // See https://stackoverflow.com/a/41081908. Note that 'git init' and
-    // 'git remote add origin' are idempotent and it's safe to run them
-    // every time we want to download new packages.
-    s"git -C $REPO_DIR init" !!;
-    s"git -C $REPO_DIR remote add origin https://github.com/rust-lang/crates.io-index" !!;
-    s"git -C $REPO_DIR fetch --depth 1" !!;
-    s"git -C $REPO_DIR reset --hard origin/master" !!
+  override def downloadMetaInformation: IO[Unit] = {
+    for {
+      _       <- IO(FileUtils.deleteDirectory(RepoDir))
+      archive <- new FileDownloader().download(GithubUrl, RepoDir.toPath / "archive.zip")
+      _       <- IO(ArchiverFactory.createArchiver(ZIP).extract(archive, RepoDir))
+    } yield ()
   }
 
   override protected def getLastVersions: Map[String, Version] = {
     val seq = Helper
-      .recursiveListFiles(REPO_DIR.toIO)
+      .recursiveListFiles(RepoDir)
       .collect {
-        case file if !(IGNORE_FILES contains file.getName) =>
+        case file if !(IgnoreFiles contains file.getName) =>
           val lastVersionJSON = scala.io.Source.fromFile(file).getLines().toSeq.last
           val obj             = Json.parse(lastVersionJSON)
           val name            = (obj \ "name").as[String]
