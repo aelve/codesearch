5e404b5aedcc4b71fc9b863ef56ee04c2b7f18a3
evgeny <evengining@yandex.ru>
2018-12-09 19:18:33 +0300

    Refactor

--- core/src/main/scala/codesearch/core/search/Search.scala
+++ core/src/main/scala/codesearch/core/search/Search.scala
@@ -15,6 +15,7 @@ import codesearch.core.util.Helper
 import codesearch.core.util.Helper.readFileAsync
 import io.chrisdavenport.log4cats.SelfAwareStructuredLogger
 import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
+import codesearch.core.regex.space.SpaceInsensitive
 
 import scala.sys.process.Process
 
@@ -65,15 +66,16 @@ trait Search {
   }
 
   private def arguments(request: SearchRequest): List[String] = {
-    def extensionsRegex   = extensions.sourceExtensions.mkString(".*\\.(", "|", ")$")
-    val forExtensions     = if (request.sourcesOnly) extensionsRegex else ".*"
-    val preciseMatchQuery = if (request.preciseMatch) Helper.preciseMatch(request.query) else request.query
-    val spaceInsensitiveQuery =
-      if (request.spaceInsensitive) Helper.spaceInsenstive(preciseMatchQuery) else preciseMatchQuery
+    def extensionsRegex: String = extensions.sourceExtensions.mkString(".*\\.(", "|", ")$")
+    val forExtensions: String   = if (request.sourcesOnly) extensionsRegex else ".*"
+    val query: String = {
+      val preciseMatch: String = if (request.preciseMatch) Helper.preciseMatch(request.query) else request.query
+      if (request.spaceInsensitive) SpaceInsensitive.spaceInsensitiveString(preciseMatch) else preciseMatch
+    }
     if (request.insensitive) {
-      List("csearch", "-n", "-i", "-f", forExtensions, spaceInsensitiveQuery)
+      List("csearch", "-n", "-i", "-f", forExtensions, query)
     } else {
-      List("csearch", "-n", "-f", forExtensions, spaceInsensitiveQuery)
+      List("csearch", "-n", "-f", forExtensions, query)
     }
   }
 
--- core/src/main/scala/codesearch/core/util/Helper.scala
+++ core/src/main/scala/codesearch/core/util/Helper.scala
@@ -40,25 +40,14 @@ object Helper {
     IO(Source.fromFile(path, "UTF-8")).bracket(source => IO.pure(source.getLines.toList))(source => IO(source.close()))
 
   def preciseMatch(query: String): String = {
-    val queryTokens: Seq[Token] = Tokenizer.parseStringWithSpecialSymbols(query)
+    val queryTokens: Seq[Token]  = Tokenizer.parseStringWithSpecialSymbols(query)
+    val specialChars: Set[String] = Set("|", "^", "$", "$", "+", "*", "(", ")", ".", "?")
     val preciseMatch: Seq[Token] = queryTokens.map {
-      case SpecialSymbol(value @ "|") => Escaped(value.charAt(0))
-      case SpecialSymbol(value @ "^") => Escaped(value.charAt(0))
-      case SpecialSymbol(value @ "$") => Escaped(value.charAt(0))
-      case SpecialSymbol(value @ "+") => Escaped(value.charAt(0))
-      case SpecialSymbol(value @ "*") => Escaped(value.charAt(0))
-      case SpecialSymbol(value @ "(") => Escaped(value.charAt(0))
-      case SpecialSymbol(value @ ")") => Escaped(value.charAt(0))
-      case SpecialSymbol(value @ ".") => Escaped(value.charAt(0))
-      case SpecialSymbol(value @ "?") => Escaped(value.charAt(0))
-      case other @ _                  => other
+      case SpecialSymbol(value) if specialChars.contains(value) => Escaped(value.charAt(0))
+      case other @ _                                            => other
     }
 
     StringAssembler.buildStringFromTokens(preciseMatch)
-  }
-
-  def spaceInsenstive(query: String): String = {
-    SpaceInsensitive.spaceInsensitiveString(query)
   }
 
   def langByLink(fileLink: String, defaultLang: String): String = {
@@ -67,20 +56,14 @@ object Helper {
   }
 
   def buildRegex(query: String, insensitive: Boolean, space: Boolean, precise: Boolean): Regex = {
-    val regex = mutable.StringBuilder.newBuilder
-    if (insensitive) {
-      regex.append("(?i)")
-    }
-    regex.append {
-      val preciseMatch = if (precise) {
-        Helper.preciseMatch(query)
-      } else {
-        query
-      }
+    val regex: String = {
+      val insensitiveCase = if (insensitive) "(?i)" else ""
+      val preciseMatch    = if (precise) Helper.preciseMatch(query) else query
+
       if (space) {
-        Helper.spaceInsenstive(preciseMatch)
+        insensitiveCase + SpaceInsensitive.spaceInsensitiveString(preciseMatch)
       } else {
-        preciseMatch
+        insensitiveCase + preciseMatch
       }
     }
     regex.toString.r
