f371decf8bd507f9094676e605485e655a9c108f
kamilongus <kamilongus@yandex.ru>
2019-01-29 16:29:05 +0400

    Fixed overhead when checking packages from the db Related to: https://github.com/aelve/codesearch/issues/194

---
+++ core/src/main/scala/codesearch/core/util/fs2/stream.scala
@@ -0,0 +1,14 @@
+package codesearch.core.util.fs2
+import cats.Functor
+import fs2.Stream
+
+object stream {
+  implicit class StreamOps[F[_]: Functor, A](val stream: Stream[F, A]) {
+
+    def filterM(f: A => F[Boolean]): Stream[F, A] =
+      stream.zip(stream.evalMap(f)).collect { case (value, true) => value }
+
+    def filterNotM(f: A => F[Boolean]): Stream[F, A] =
+      stream.zip(stream.evalMap(f)).collect { case (value, false) => value }
+  }
+}
