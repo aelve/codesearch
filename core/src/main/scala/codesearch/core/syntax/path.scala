444588656b15365a918a1a6dc0c8ecf40f1c5e7e
kamilongus <kamilongus@yandex.ru>
2019-01-29 19:49:36 +0400

    Fixes Related to: https://github.com/aelve/codesearch/issues/194

--- core/src/main/scala/codesearch/core/syntax/path.scala
+++ core/src/main/scala/codesearch/core/syntax/path.scala
@@ -2,7 +2,7 @@ package codesearch.core.syntax
 import java.nio.file.{Path, Paths}
 
 object path {
-  implicit final class RichNioPath(private val parent: Path) {
+  implicit final class RichNioPath(private val parent: Path) extends AnyVal {
     def /(child: Path): Path   = Paths.get(parent.toFile.getPath, child.toFile.getPath)
     def /(child: String): Path = Paths.get(parent.toFile.getPath, child)
   }
--- core/src/main/scala/codesearch/core/syntax/stream.scala
+++ core/src/main/scala/codesearch/core/syntax/stream.scala
@@ -1,9 +1,8 @@
 package codesearch.core.syntax
-import cats.Functor
 import fs2.Stream
 
 object stream {
-  implicit class StreamOps[F[_]: Functor, A](val stream: Stream[F, A]) {
+  implicit class StreamOps[F[_], A](val stream: Stream[F, A]) extends AnyVal {
     def filterM(f: A => F[Boolean]): Stream[F, A] =
       stream.zip(stream.evalMap(f)).collect { case (value, true) => value }
 
