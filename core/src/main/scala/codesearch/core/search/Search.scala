0e34b0a64deafbe96f99c674a6fa4312107fa7c1
Evgeny <evengining@yandex.ru>
2019-03-14 19:53:03 +0300

    [#236] flag removed in csearch arguments (#237)
    * Case insensitive arguments now removed in csearch query
    
    * Format
    
    * Edited regular expression construct and fixed import in Search.scala

--- core/src/main/scala/codesearch/core/search/Search.scala
+++ core/src/main/scala/codesearch/core/search/Search.scala
@@ -15,8 +15,7 @@ import codesearch.core.util.Helper.readFileAsync
 import fs2.{Pipe, Stream}
 import io.chrisdavenport.log4cats.SelfAwareStructuredLogger
 import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
-import codesearch.core.regex.space.SpaceInsensitiveString
-import codesearch.core.regex.PreciseMatch
+import codesearch.core.regex.RegexConstructor
 
 import scala.sys.process.Process
 
@@ -84,16 +83,12 @@ trait Search {
       case None           => if (request.sourcesOnly) extensionsRegex else ".*"
     }
 
-    val query: String = {
-      val preciseMatch: String = if (request.preciseMatch) PreciseMatch(request.query) else request.query
-      if (request.spaceInsensitive) SpaceInsensitiveString(preciseMatch) else preciseMatch
-    }
+    val query: String =
+      RegexConstructor(request.query, request.insensitive, request.spaceInsensitive, request.preciseMatch)
 
     request.filter match {
-      case Some(filter) if request.insensitive => List("csearch", "-n", "-i", "-f", forExtensions, query, filter)
-      case Some(filter)                        => List("csearch", "-n", "-f", forExtensions, query, filter)
-      case None if request.insensitive         => List("csearch", "-n", "-i", "-f", forExtensions, query)
-      case None                                => List("csearch", "-n", "-f", forExtensions, query)
+      case Some(filter) => List("csearch", "-n", "-f", forExtensions, query, filter)
+      case None         => List("csearch", "-n", "-f", forExtensions, query)
     }
   }
 
